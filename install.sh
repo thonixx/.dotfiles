c5177810c67c821a7fe99b05b7cd4d4a9c468431
Michael Tanner <git@pixelwolf.ch>
2013-08-01 01:03:45 +0200

    added ignore=dirty option in install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ home=$(echo $HOME)
 ln -s $dir/.vimrc $home/vimrc && echo "symlinked to $home/.vimrc"
 git submodule init && echo "initialized submodules"
 git submodule update && echo "updated submodules"
+sed -i.bak "s/tlib.git$/tlib.git\n\tignore = dirty/g" .git/config && echo "added ignore option to tlib submodule"
 
 echo "vim should be ready to use. maybe install the 256 color ability for your terminal (ncurses-term)."
