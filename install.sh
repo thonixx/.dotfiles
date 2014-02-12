2a6e2de12ed9849890a6d497765301fd9bfceaff
Michael Tanner <git@pixelwolf.ch>
2014-02-12 18:05:57 +0100

    added gitconfig to the repo
    also has gpg encrypted personal informations
    (name and mail address) to be decrypted while
    installation via install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 # define directory where zsh is stored
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# ask whether use personal or work settings
+echo -n "type 'personal' or 'work' and press enter: "
+read env
+
+# check for input
+if [ -z "$env" ]; then echo "no input given. cancelling script."; exit 1; fi
+
 # check for env var HOME or assume /home/USER
 if [ "$HOME" ]
 then
@@ -30,8 +37,17 @@ else
 	mv $home/.tmux.conf $home/.tmux.conf.bak && echo ".tmux.conf backed up."
 fi
 
+# backup gitconfig if file exists or unlink it
+if [ -h "$home/.gitconfig" ]
+then
+	unlink $home/.gitconfig && echo ".gitconfig is now unlinked."
+else
+	mv $home/.gitconfig $home/.gitconfig.bak && echo ".gitconfig backed up."
+fi
+
 # link zshrc
 ln -s $dir/zshrc $zshrc && echo ".zshrc is now installed"
+
 # link tmux.conf
 if [ -f "$dir/tmux.conf.local" ]
 then
@@ -40,5 +56,23 @@ else
 	ln -s $dir/tmux.conf $home/.tmux.conf && echo ".tmux.conf is now installed"
 fi
 
+# copy gitconfig.$type
+case $env in
+	'work')
+		type='work'
+	;;
+	'personal'|*)
+		type='personal'
+	;;
+esac
+# decrypt and save it
+gpg < $dir/gitconfig.$type.gpg > $home/.gitconfig && echo ".gitconfig.$type is now installed"
+# append gitconfig content
+cat $dir/gitconfig >> $home/.gitconfig && echo ".gitconfig content appended"
+
 # updating submodules
 git submodule init > /dev/null && git submodule update > /dev/null && echo "configured submodules"
+
+# end script
+echo "script ended"
+exit 0
