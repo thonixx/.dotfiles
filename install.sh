2344ab9c7b0953c36e3d9be2b0faf8a1a14b86ab
Michael Tanner <michael.tanner@dswiss.com>
2016-07-06 14:30:06 +0200

    fix "ingore = dirty" part

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ home=$(echo $HOME)
 ln -s $dir/vimrc $home/.vimrc && echo "symlinked to $home/.vimrc"
 git submodule init && echo "initialized submodules"
 git submodule update && echo "updated submodules"
-sed -i.bak "s/tlib.git$/tlib.git\n\tignore = dirty/g" .git/config && echo "added ignore option to tlib submodule"
+sed -i "s/tlib.git$/tlib.git
+	ignore = dirty/g" .git/config && echo "added ignore option to tlib submodule"
 touch $dir/vimrc.local
 
 echo "vim should be ready to use. maybe install the 256 color ability for your terminal (ncurses-term)."
