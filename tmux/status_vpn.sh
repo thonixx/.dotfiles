bbb3d44eb17221566b7bbf030f8683f2320d8a4f
Michael Tanner <michael.tanner@dswiss.com>
2018-06-07 12:25:04 +0200

    add vpn status line

---
+++ tmux/status_vpn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# check if vpn is running or not
+
+# no matter which locale, display it with C
+export LC_ALL=C
+
+# get status of vpn
+test -f "$(which openfortivpn)" || exit 0
+
+pgrep -qa openfortivpn \
+    && {
+        colour="#[fg=black]#[bg=green]"
+        fvpn="VPN up"
+    } \
+    || {
+        colour="#[fg=white]#[bg=red]"
+        fvpn="VPN down"
+    }
+
+# print it centered and nicely
+printf "$colour%5s%-5s#[default]\n" "$(echo "$fvpn" | cut -c 1-$((${#fvpn}/2)))" "$(echo "$fvpn" | cut -c $((${#fvpn}/2+1))-${#fvpn})"
