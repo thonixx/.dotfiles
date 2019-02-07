94f87dafe65367708d14ea49540402f50876a03c
Michael Tanner <git@pixelwolf.ch>
2019-02-07 19:07:58 +0100

    something like a "default" for tmux IP status

--- tmux/status_ip.sh
+++ tmux/status_ip.sh
@@ -9,5 +9,8 @@ uname | grep -q Darwin \
     && outgoing_ip="$(netstat -rn | grep default | awk '{print $NF}' | head -n1 | xargs ifconfig | grep inet | grep -ve 127.0.0.1 -e fe80: | sed -E 's/^.*inet6? ([^ ]+).*$/\1/' | head -n1)" \
     || outgoing_ip="$(netstat -rn | grep "^0.0.0.0" | awk '{print $NF}' | xargs -n1 ifconfig | grep inet | head -n1 | grep -Eo "inet ([0-9]{1,3}.){3}[0-9]{1,3}" | awk '{print $NF}' | head -n1)"
 
+# default when no IP
+test "x$outgoing_ip" == "x" && outgoing_ip="no_ip"
+
 # print it centered and nicely
 printf '%6s%-7s\n' $(echo $outgoing_ip | cut -c 1-$((${#outgoing_ip}/2))) $(echo $outgoing_ip | cut -c $((${#outgoing_ip}/2+1))-${#outgoing_ip})
