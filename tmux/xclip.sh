93e0bcb864cb3606c6fb425d8a6d5657f13ddbb7
Michael Tanner <michael.tanner@dswiss.com>
2020-06-24 16:04:05 +0200

    tell me about the status

--- tmux/xclip.sh
+++ tmux/xclip.sh
@@ -3,3 +3,6 @@
 # has to be in a script as otherwise tmux freezes as long as xclip is running
 exec > /dev/null # without that xclip will block tmux
 tmux show-buffer | xclip -i
+
+# tell me about the status
+test $? -eq 0 && tmux display-message 'Copied to xclipboard!' || tmux display-message "Error ($?) while copying tmux buffer to xclip!"
